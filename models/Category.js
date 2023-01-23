0f248032a7f71589b4d73735c325bf32ebea3764
kelvinEskca <beredugokelvin@gmail.com>
2023-01-23 20:01:52 +0100

    update

--- models/Category.js
+++ models/Category.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const categorySchema = new mongoose.Schema({
     name:{type:String,required:true,unique:true},
-    subcategory:{type:String,required:true,unique:true},
+    subcategory:{type:String,required:true},
     desc:{type:String,required:true},
 },{timestamps:true})
 
